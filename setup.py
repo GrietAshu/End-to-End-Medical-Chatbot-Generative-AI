44231e50f2d141a2b97bb3398d1bc54e1936e4d9
GrietAshu <ashish22241a6625@grietcollege.com>
2025-04-20 22:32:10 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'GENERATIVE AI Project',
+    version = '0.0.0',
+    author = 'Bappy Ahmed',
+    author_email = 'entbappy73@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
